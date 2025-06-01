32c39d5b69cc998ffc9e64ac0f13a0ccbe80ef44
Peter Kovecses <kovecsespeter@gmail.com>
2025-06-01 17:34:20 +0200

    Remove redundant error handling from query

--- src/DapperDemo.Api/Endpoints/Product/GetProducts/GetProductQuery.cs
+++ src/DapperDemo.Api/Endpoints/Product/GetProducts/GetProductQuery.cs
@@ -14,17 +14,7 @@ public static class GetProductQuery
                            JOIN product_categories c ON p.category_id = c.id
                            """;
 
-        IEnumerable<ProductListViewDto> products;
-        
-        try
-        {
-            products = await dbConnection.QueryAsync<ProductListViewDto>(sql);
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e);
-            throw;
-        }
+        var products = await dbConnection.QueryAsync<ProductListViewDto>(sql);
         
         return Results.Ok(products);
     }
